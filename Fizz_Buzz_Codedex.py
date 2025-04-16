0d63d8c7efcc89dbb88f83cfa0b7cbcde139b2fd
Charlotte <CharlotteGov@gmail.com>
2025-04-16 19:31:28 +0100

    Add files via upload

---
+++ Fizz_Buzz_Codedex.py
@@ -0,0 +1,38 @@
+for i in range(1, 101):
+  if i % 3 == 0 and i % 5 == 0:
+    print("FizzBuzz")
+  elif i % 5 == 0:
+    print("Buzz")
+  elif i % 3 == 0:
+    print("Fizz")
+  else:
+    print(i)
+  
+  
+  #while (i*3 for i in range (1, 100)):
+    #print("fizz")
+  #elif (i*5 for i in range (1, 100)):
+    #print("buzz")
+  #elif (i*3*5 for i in range (1,100)):
+    #print("Fizzbuzz")
+  #else:
+    #print(i)
+
+#buzz = [i*5 for i in range (1, 100)]
+
+#print(buzz)
+
+#fizzbuzz = [i*3*5 for i in range(1, 100)]
+
+#print(fizzbuzz)
+
+#while fizz:
+  #print("Fizz")
+
+#while buzz:
+  #print("Buzz")
+
+#while fizzbuzz:
+  #print("FizzBuzz")
+
+
